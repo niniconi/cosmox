3468f8d9095f6bc6a953f662b6adaaeb0d6be03b
niniconi <Stump0828Provoking2393Y@gmail.com>
2025-12-05 21:49:40 +0800

    refactor: Use crate-relative paths in the log macro

--- cosmox-api/src/api.rs
+++ cosmox-api/src/api.rs
@@ -2,8 +2,8 @@ pub mod log {
   #[macro_export]
   macro_rules! error {
     ($($arg:tt)*) => {
-      cosmox::plugin::cosmox_api::log(
-        cosmox::plugin::cosmox_types::LogLevel::Error,
+      crate::cosmox::plugin::cosmox_api::log(
+        crate::cosmox::plugin::cosmox_types::LogLevel::Error,
         &format!($($arg)*),
       );
     }
@@ -12,8 +12,8 @@ pub mod log {
   #[macro_export]
   macro_rules! warn {
     ($($arg:tt)*) => {
-      cosmox::plugin::cosmox_api::log(
-        cosmox::plugin::cosmox_types::LogLevel::Warn,
+      crate::cosmox::plugin::cosmox_api::log(
+        crate::cosmox::plugin::cosmox_types::LogLevel::Warn,
         &format!($($arg)*),
       )
     }
@@ -22,8 +22,8 @@ pub mod log {
   #[macro_export]
   macro_rules! info {
     ($($arg:tt)*) => {
-      cosmox::plugin::cosmox_api::log(
-        cosmox::plugin::cosmox_types::LogLevel::Info,
+      crate::cosmox::plugin::cosmox_api::log(
+        crate::cosmox::plugin::cosmox_types::LogLevel::Info,
         &format!($($arg)*),
       );
     }
@@ -32,8 +32,8 @@ pub mod log {
   #[macro_export]
   macro_rules! debug {
     ($($arg:tt)*) => {
-      cosmox::plugin::cosmox_api::log(
-        cosmox::plugin::cosmox_types::LogLevel::Debug,
+      crate::cosmox::plugin::cosmox_api::log(
+        crate::cosmox::plugin::cosmox_types::LogLevel::Debug,
         &format!($($arg)*),
       );
     }
@@ -42,8 +42,8 @@ pub mod log {
   #[macro_export]
   macro_rules! trace {
     ($($arg:tt)*) => {
-      cosmox::plugin::cosmox_api::log(
-        cosmox::plugin::cosmox_types::LogLevel::Trace,
+      crate::cosmox::plugin::cosmox_api::log(
+        crate::cosmox::plugin::cosmox_types::LogLevel::Trace,
         &format!($($arg)*),
       );
     }
@@ -52,8 +52,8 @@ pub mod log {
   #[macro_export]
   macro_rules! fatal {
     ($($arg:tt)*) => {
-      cosmox::plugin::cosmox_api::log(
-        cosmox::plugin::cosmox_types::LogLevel::Fatal,
+      crate::cosmox::plugin::cosmox_api::log(
+        crate::cosmox::plugin::cosmox_types::LogLevel::Fatal,
         &format!($($arg)*),
       );
     }
